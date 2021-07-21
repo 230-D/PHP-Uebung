1fceca58c2e4ab53541174c736ec5341e20ea548
230-D <72095025+230-D@users.noreply.github.com>
2021-07-21 18:58:25 +0200

    update

--- PHP/schleifen_wiederholung_aufgaben_.php
+++ PHP/schleifen_wiederholung_aufgaben_.php
@@ -16,13 +16,14 @@
 // Schreibe eine for-Schleife die von 0 bis 100 zählt und die aktuelle Itteration
 // untereinander ausgibt.
 
+
 /*
-$i=0;
 
 for ($i=0; $i <= 100; $i++)
 {
-    echo $i . '<br>';
-}
+    echo $i . '<br>';           // alternativ: echo "$i <br>"; 
+                              // Single '' Quotes werden alle Daten zu Strings
+}                           // Double "" Quotes werden bestimmte Daten zu Strings
 */
 
  // Schreibe eine while-Schleife die von 50 bis -50 zählt und die aktuelle Itteration
@@ -42,10 +43,10 @@ while ($i >= -50)
  // Schreibe eine foreach-Schleife die ein assoziatives Array mit key und value untereinander ausgibt.
  // Verwende single-quotes im echo-statement.
 
- /*
+/*
 $Fahrray =
 [
-    'Fahrrad1' => 1800,
+    'Fahrrad1' => 1800,         // => lautet Fieldarrow
     'Fahrrad2' => 2000,
     'Fahrrad3' => 500,
     'Fahrrad4' => 800
@@ -55,8 +56,8 @@ foreach ($Fahrray as $key => $value) {
     echo $key .' '. $value;
     echo '<br>';
 }
-*/
 
+*/
 
  // Schreibe eine do...while-Schleife die von 0 bis 19 alles Zahlen untereinander ausgibt.
  // Verwende in der Bedingung nur den kleiner-als-Operator
@@ -68,7 +69,17 @@ do {
     echo $i++;
     echo '<br>';
 } while ($i < 20);
+
+
 */
+
+/*      Alternativ
+
+$a
+
+
+*/
+
 
 
  // Push dieses File in dein GitHub-Repository
